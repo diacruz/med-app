eb73b4e9230b858a8222c3f22b2fcdb485a30b0e
Yeilys <yfund005@fiu.edu>
2020-06-19 02:32:22 -0400

    o	edited profile to display a more professional look o	set a default avatar image to show in the profile if none is set o	added several effects to the components of the profile to make them stand out.

---
+++ models/profile.js
@@ -0,0 +1,13 @@
+class Profile{
+    constructor(id, name, email, number, title, avatar, avatarBackground){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.number = number;
+        this.title = title;
+        this.avatar = avatar;
+        this.avatarBackground = avatarBackground;
+    }
+}
+
+export default Profile;
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
-import { Ionicons } from '@expo/vector-icons';
-import { View, Button, Text, StyleSheet, Platform, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { Ionicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Dropdown } from 'react-native-material-dropdown';
+import {
+    View, SafeAreaView, ScrollView, Button, Text, StyleSheet, Platform, Image, TouchableOpacity, ImageBackground
+} from 'react-native';
 import * as firebase from 'firebase'
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import * as ImagePicker from 'expo-image-picker';
 import UserPermission from '../utilities/UserPermission';
 import EditProfileScreen from '../screens/EditProfileScreen';
+import Colors from '../constants/Colors';
 
-
-
-class ProfileScreen extends React.Component {
+class ProfileScreen extends Component {
     state = {
         email: "",
         displayName: "",
-        avatar: "",
-    };
-
-    componentDidMount() {
-        const { email, displayName } = firebase.auth().currentUser;
-        this.setState({ email, displayName });
+        avatar: '',
+        showDefault: true,
+        status: '',
     };
 
     handlePickAvatar = async () => {
@@ -33,135 +32,211 @@ class ProfileScreen extends React.Component {
         })
         if (!result.cancelled) {
             this.setState({ avatar: result.uri })
+            this.setState({ showDefault: false })
         }
+
     };
 
+    componentDidMount() {
+        const { email, displayName } = firebase.auth().currentUser;
+        this.setState({ email, displayName });
+
+    };
+
+
+
     render() {
+
+        var image = this.state.showDefault ? require('../components/img/default-profile-pic.jpg') : { uri: this.state.avatar };
         return (
-            <View>
-                <View><ImageBackground style={styles.header} source={require('../components/img/headerbg.jpg')} /></View>
-                <TouchableOpacity style={styles.avatarPlaceholder} onPress={this.handlePickAvatar}>
-                    <Image source={{ uri: this.state.avatar }} style={styles.avatar} />
-                    <Ionicons name="ios-add" size={30} color="#aFFF"
-                        style={{ marginTop: 6, marginLeft: 2 }}>
-                    </Ionicons>
-                </TouchableOpacity>
-                <Text style={styles.name}>Name</Text>
-                <Text style={styles.info}>Title</Text>
-                <View style={styles.body}>
-                    <View style={styles.bodyContent}>
-                        <Text style={styles.title}>Email Address: </Text>
-                        <Text style={styles.email}>{this.state.email}</Text>
-                        <Text style={styles.title}>Phone Number: </Text>
-                        <Text style={styles.description}>#</Text>
+            <SafeAreaView style={styles.container}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                        <View style={{ alignSelf: "center" }}>
+                            <View style={styles.profileImage}>
+                                <Image source={image} style={styles.avatar} resizeMode="cover"></Image>
+                            </View>
+                            <View style={styles.active}></View>
+                            <View style={styles.add}>
+                                <Ionicons name="ios-add" size={30} color="#DFD8C8" style={{ marginTop: 0, marginLeft: 2 }} onPress={this.handlePickAvatar}></Ionicons>
+                            </View>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={[styles.text, { fontWeight: "200", fontSize: 20, fontWeight: "bold" }]}>Juanita Gonzalez</Text>
+                            <Text style={[styles.text, { color: "#AEB5BC", fontSize: 16 }]}>Title</Text>
+                        </View>
+
+                    <View style={styles.statusContainer}>
+                        <View style={styles.status}>
+                            <TouchableOpacity style={{ alignItems: "center" }}>
+                                <MaterialCommunityIcons name="emoticon-happy-outline" size={20}></MaterialCommunityIcons>
+                                <Text>Set Status</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.status}>
+                            <TouchableOpacity style={{ alignItems: "center" }} onPress={() => this.props.navigation.navigate('Edit')}>
+                                <MaterialIcons name="edit" size={20}></MaterialIcons>
+                                <Text>Edit Profile</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.status}>
+                            <TouchableOpacity style={{ alignItems: "center" }}>
+                                <MaterialIcons name="more-horiz" size={20}></MaterialIcons>
+                                <Text>More</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={styles.container}>
-                        <TouchableOpacity style={styles.buttonStyle} onPress={() => this.props.navigation.navigate('Edit')}>
-                            <Text style={{ color: 'white', textAlign: 'center' }}>Edit Profile</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonStyle2} >
-                            <Text style={{ color: 'white', textAlign: 'center' }}>Logout</Text>
+
+
+
+                    <View style={[styles.detailContainer]}>
+                        <View style={styles.iconBox}>
+                            <MaterialIcons name="email" size={20}></MaterialIcons>
+                        </View>
+                        <View style={styles.detailBox}>
+                            <Text style={[styles.text, { fontSize: 16 }]}>Email Address: </Text>
+                            <Text style={[styles.text, styles.subText]}>{this.state.email}</Text>
+                        </View>
+                    </View>
+                    <View style={[styles.detailContainer]}>
+                        <View style={styles.iconBox}>
+                            <MaterialIcons name="local-phone" size={20}></MaterialIcons>
+                        </View>
+                        <View style={styles.detailBox}>
+                            <Text style={[styles.text, { fontSize: 16 }]}>Phone Number: </Text>
+                            <Text style={[styles.text, styles.subText]}>786-234-4567</Text>
+                        </View>
+                    </View>
+                    <View style={[styles.detailContainer]}>
+                        <View style={styles.iconBox}>
+                            <MaterialCommunityIcons name="certificate" size={20}></MaterialCommunityIcons>
+                        </View>
+                        <View style={styles.detailBox}>
+                            <Text style={[styles.text, { fontSize: 16 }]}>Certifications:</Text>
+                            <Text style={[styles.text, styles.subText]}>Show All ></Text>
+                        </View>
+                    </View>
+                    <View style={styles.buttonStyle}>
+                        <TouchableOpacity onPress={this.handlPress}>
+                            <Text style={styles.button}>LOGOUT</Text>
                         </TouchableOpacity>
                     </View>
-                </View>
-            </View>
+                </ScrollView>
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 250,
-    },
-    switch: {
-        alignItems: "center",
-        justifyContent: "center"
-    },
     container: {
-        marginTop: 70,
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        flex: 1,
+        backgroundColor: "#fff"
     },
-    title: {
-        marginTop: 30,
-        fontSize: 17,
-        color: "black",
-        fontWeight: '600',
-    },
-    buttonStyle: {
-        justifyContent: 'center',
-        width: 100,
-        height: 50,
-        backgroundColor: 'pink',
-        borderRadius: 30,
-        marginBottom: 5
-    },
-    buttonStyle2: {
-        justifyContent: 'center',
-        width: 80,
-        height: 50,
-        backgroundColor: 'pink',
-        borderRadius: 30,
+    text: {
+        fontFamily: "HelveticaNeue",
+        color: "#52575D",
     },
     avatar: {
-        width: 120,
-        height: 120,
-        borderRadius: 100,
-        borderColor: "white",
-        marginBottom: 10,
-        alignSelf: 'center',
-        position: 'absolute',
-        marginTop: 30
+        flex: 1,
+        width: null,
+        height: null,
     },
-    avatarPlaceholder: {
-        borderRadius: 100,
-        width: 120,
-        height: 120,
-        backgroundColor: "#E1E2E6",
-        marginTop: 60,
-        marginBottom: 10,
-        justifyContent: "center",
-        alignSelf: 'center',
-        alignItems: "center",
-        position: 'absolute',
-    },
-    email: {
-        fontStyle: "italic",
-        fontSize: 15,
-        color: "#00BFFF",
-        marginTop: 10
-    },
-    body: {
+    profileImage: {
+        width: 150,
+        height: 150,
+        borderRadius: 80,
+        overflow: "hidden",
         marginTop: 0,
-        height: '100%',
+    },
+    active: {
+        backgroundColor: "#34FFB9",
+        position: "absolute",
+        bottom: 20,
+        left: 5,
+        padding: 4,
+        height: 20,
+        width: 20,
+        borderRadius: 10
+    },
+    add: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        right: 8,
+        bottom: 13,
+        width: 30,
+        height: 30,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    infoContainer: {
+        alignSelf: "center",
+        alignItems: "center",
+        marginTop: 8,
+        marginBottom: 15
+    },
+    detailContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        height: 60,
+        alignSelf: "center",
+        marginTop: 15,
+        marginHorizontal: 25,
         backgroundColor: "white",
+        shadowColor: "gray",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
     },
-    bodyContent: {
-        alignItems: 'center',
+    status: {
+        flex: 1,
+        alignItems: "center",
     },
-    name: {
-        fontSize: 23,
-        color: "white",
-        fontWeight: '600',
-        marginTop: 185,
-        position: 'absolute',
-        alignSelf: 'center',
+    statusContainer: {
+        flexDirection: "row",
+        alignSelf: "center",
+        marginBottom: 10,
+        width: 270,
     },
-    info: {
+    iconBox: {
+        flex: 0.3,
+        alignItems: "flex-end"
+    },
+    detailBox: {
+        flex: 1,
+        alignItems: "center",
+        borderColor: "silver",
+        borderLeftWidth: 1,
+        marginLeft: 40
+    },
+    subText: {
         fontSize: 14,
-        color: "white",
-        fontWeight: '600',
-        marginTop: 220,
-        position: 'absolute',
-        alignSelf: 'center',
+        color: "#AEB5BC",
+        textTransform: "uppercase",
+        fontWeight: "500",
+        marginTop: 5
     },
-    description: {
-        fontSize: 16,
-        color: "#696969",
-        marginTop: 10,
-        textAlign: 'center'
+    buttonStyle: {
+        marginTop: 30,
+        alignContent: "center",
+        alignSelf: "center"
     },
-
+    button: {
+        backgroundColor: Colors.primaryColor,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 20,
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+        overflow: 'hidden',
+        padding: 12,
+        textAlign: 'center',
+        width: 90,
+    }
 });
 
 ProfileScreen.navigationOptions = navigationdata => {
